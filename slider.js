0024bb7a31f73ce4451ad3a86a6a5ee3bfecd45e
Sdney Fernandes <fsidney987@gmail.com>
2023-10-31 21:52:47 -0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,17 @@
+
+/*
+var radio =document.querySelector('.manual-btn')
+document.getElementById('radio1').checked =true
+var cont = 1
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+cont++
+if(cont > 4){
+    cont = 1
+}
+document.getElementById('radio' + cont).checked =true
+}
+*/
